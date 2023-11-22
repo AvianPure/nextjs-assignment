4662cf06b4fd2d93d38d6a31ba1d2a6440e7a238
AvianPure <129159138+AvianPure@users.noreply.github.com>
2023-11-22 09:01:54 +0200

    new file:   src/app/components/navbar.tsx 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,10 @@
+export function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Koti</a></li>
+                <li><a href="/hinnat">Hinnat</a></li>
+            </ul>
+        </nav>
+    );
+}
